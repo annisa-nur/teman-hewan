1faebfc88a71befe24db9986b000a0e768e1ec31
Annisa Nur <85719079+annisa-nur@users.noreply.github.com>
2023-05-28 16:35:06 +0700

    Add files via upload

---
+++ src/components/Problem.js
@@ -0,0 +1,22 @@
+import React from "react";
+import { Component } from "react";
+
+export default class Mission extends Component {
+    render(){
+        return (
+            <section className="head">
+                <div className="cart-info">
+                    <div className="head">
+                    <article className="desc-mis">
+                        <h3>Our Background</h3>
+                        <p>Street art edison bulb gluten-free, tofu try-hard lumbersexual brooklyn tattooed pickled chambray. Actually humblebrag next level, deep v art party wolf tofu direct trade readymade sustainable hell of banjo. Organic authentic subway tile cliche palo santo, street art XOXO dreamcatcher retro sriracha portland air plant kitsch stumptown. Austin small batch squid gastropub. Pabst pug tumblr gochujang offal retro cloud bread bushwick semiotics before they sold out sartorial literally mlkshk. Vaporware hashtag vice, sartorial before they sold out pok pok health goth trust fund cray.</p>
+                    </article>
+                    <article className="info-mis">
+                        <img src="" alt=""></img>
+                    </article>
+                    </div>
+                </div>
+            </section>
+        )
+    }
+}
